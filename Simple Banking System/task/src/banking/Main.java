fbf201c9e7e8b7168def113e517b5812315a8a55
gfton <gftonot@gmail.com>
2022-07-15 21:36:48 +0300

    data base

--- Simple Banking System/task/src/banking/Main.java
+++ Simple Banking System/task/src/banking/Main.java
@@ -1,5 +1,10 @@
 package banking;
 
+import org.sqlite.SQLiteDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,9 +12,20 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         ArrayList<Account> accounts = new ArrayList<>();
 
+        //Connecting to database
+        String url = "jdbc:sqlite:" + args[1];
+
+        SQLiteDataSource dataSource = new SQLiteDataSource();
+        dataSource.setUrl(url);
+        try {
+            Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS card(id INTEGER,number TEXT,pin TEXT,balance INTEGER DEFAULT 0)");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         label:
         while (true) {
             System.out.println("1. Create an account\n" +
@@ -22,6 +38,15 @@ public class Main {
                 case "1": {
                     Account account = new Account();
                     accounts.add(account);
+
+                    try {
+                        Connection connection = dataSource.getConnection();
+                        Statement statement = connection.createStatement();
+                        statement.executeUpdate(String.format("INSERT INTO card(number, pin, balance) VALUES (%d, %s, %d)"
+                                , account.getCardNumber(), account.getPin(), account.getBalance()));
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
 
                     System.out.println("Your card has been created");
                     System.out.println("Your card number:\n" + account.getCardNumber());
@@ -36,15 +61,14 @@ public class Main {
 
                     Account loggedAccount = null;
 
-                    for (Account account: accounts) {
-                        if(account.getCardNumber() == cardNumber && account.getPin().equals(pin)) {
-                            loggedAccount =account;
+                    for (Account account : accounts) {
+                        if (account.getCardNumber() == cardNumber && account.getPin().equals(pin)) {
+                            loggedAccount = account;
                         }
                     }
 
                     if (loggedAccount != null) {
                         System.out.println("You have successfully logged in!");
-                        boolean toBreak = false;
 
                         label1:
                         while (true) {
@@ -61,14 +85,9 @@ public class Main {
                                     System.out.println("You have successfully logged out!");
                                     break label1;
                                 case "0":
-                                    toBreak = true;
                                     System.out.println("Bye!");
-                                    break label1;
+                                    break label;
                             }
-                        }
-
-                        if (toBreak) {
-                            break label;
                         }
 
                     } else {
